b776c9f7a1540e0e39ae0aa760abe75433c3bc2c
DavidBerz <davids.berz@gmail.com>
2023-10-02 00:56:00 -0600

    add components header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { footerComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    footerComponent
   ],
   imports: [
     BrowserModule
