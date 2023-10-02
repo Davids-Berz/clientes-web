1e4a39444fa10427aa8de361c630ccf1f0950318
DavidBerz <davids.berz@gmail.com>
2023-10-02 02:38:13 -0600

    se agrega file cliente json

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {Clientes} from './clientes';
+import {Cliente} from './cliente';
+import {CLIENTES} from './cliente.json';
 
 @Component({
   selector: 'app-clientes',
@@ -11,25 +12,10 @@ export class ClientesComponent implements OnInit {
   constructor() {
   }
 
-  clientes: Clientes[] = [
-    {id: 1, nombre: 'David', apellido: 'Saldivar', createAt: '2023-01-01', email: 'Davids.berz@gmail.com'},
-    {id: 2, nombre: 'Valeria', apellido: 'Perez', createAt: '2022-08-14', email: 'valeria.p@email.com'},
-    {id: 3, nombre: 'Carlos', apellido: 'García', createAt: '2023-05-20', email: 'c.garcia@email.com'},
-    {id: 4, nombre: 'Mateo', apellido: 'Gomez', createAt: '2023-01-25', email: 'mateo.g@email.com'},
-    {id: 5, nombre: 'Isabella', apellido: 'Lopez', createAt: '2022-12-10', email: 'isabella.l@email.com'},
-    {id: 6, nombre: 'Daniel', apellido: 'Torres', createAt: '2023-04-03', email: 'daniel.t@email.com'},
-    {id: 7, nombre: 'Sophia', apellido: 'Martinez', createAt: '2022-11-15', email: 'sophia.m@gmail.com'},
-    {id: 8, nombre: 'Javier', apellido: 'Hernández', createAt: '2023-03-17', email: 'javier.h@email.com'},
-    {id: 9, nombre: 'Alejandro', apellido: 'Ramírez', createAt: '2023-02-28', email: 'ale.ramirez@email.com'},
-    {id: 10, nombre: 'Gabriela', apellido: 'Diaz', createAt: '2022-10-05', email: 'gabriela.d@email.com'},
-    {id: 11, nombre: 'Lucia', apellido: 'Vega', createAt: '2022-11-30', email: 'lucia.v@email.com'},
-    {id: 12, nombre: 'Laura', apellido: 'Fernández', createAt: '2022-09-08', email: 'laura.fern@gmail.com'},
-    {id: 13, nombre: 'Sofia', apellido: 'Rojas', createAt: '2022-07-19', email: 'sofia.r@email.com'},
-    {id: 14, nombre: 'Diego', apellido: 'Santos', createAt: '2023-06-22', email: 'diego.s@email.com'},
-    {id: 15, nombre: 'Eduardo', apellido: 'Mendoza', createAt: '2023-08-12', email: 'eduardo.m@email.com'}
-  ];
+  clientes: Cliente[];
 
   ngOnInit() {
+    this.clientes = CLIENTES;
   }
 
 }
