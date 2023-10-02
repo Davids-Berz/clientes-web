e5455589ec31ccee62a01c16d6deea1599eb33d7
DavidBerz <davids.berz@gmail.com>
2023-10-02 02:30:14 -0600

    add component cliente mas lista de clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { footerComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
+import { ClientesComponent } from './clientes/clientes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     footerComponent,
-    DirectivaComponent
+    DirectivaComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule
