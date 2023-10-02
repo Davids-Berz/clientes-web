ef93c13a9b1327ac595c7e4e0eac7721f6b25a6e
DavidBerz <davids.berz@gmail.com>
2023-10-02 06:25:37 -0600

    implementando servicio cliente

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,18 +1,27 @@
 import {Injectable} from '@angular/core';
-import {CLIENTES} from './cliente.json';
 import {Cliente} from './cliente';
-import {Observable, of} from 'rxjs';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {map, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
+  private urlEndPoint = 'http://localhost:8080/api/clientes';
+
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    // return of(CLIENTES);
+    // return this.http.get<Cliente[]>(this.urlEndPoint);
+    return this.http.get(this.urlEndPoint).pipe(
+      map(response => response as Cliente[])
+    ).pipe(
+      tap(response => console.log(response))
+    );
   }
 
 }
