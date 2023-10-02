81c002b9615e42186c5be48cbaadac1456ffb1d7
DavidBerz <davids.berz@gmail.com>
2023-10-02 03:38:32 -0600

    agregando rutas y navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { footerComponent } from './footer/footer.component';
-import { DirectivaComponent } from './directiva/directiva.component';
-import { ClientesComponent } from './clientes/clientes.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {footerComponent} from './footer/footer.component';
+import {DirectivaComponent} from './directiva/directiva.component';
+import {ClientesComponent} from './clientes/clientes.component';
 import {ClienteService} from './clientes/cliente.service';
+import {RouterModule, Routes} from '@angular/router';
+
+// Rutas
+const routes: Routes = [
+  {path: '', redirectTo: '/clientes', pathMatch: 'full'},
+  {path: 'directivas', component: DirectivaComponent},
+  {path: 'clientes', component: ClientesComponent}
+];
+
 
 @NgModule({
   declarations: [
@@ -16,11 +25,14 @@ import {ClienteService} from './clientes/cliente.service';
     DirectivaComponent,
     ClientesComponent
   ],
+  // Se registran los Module
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   // registrar los Service en providers
   providers: [ClienteService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
